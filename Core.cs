c92e1cd05fc8d5db0924d84739de37b25fc9bb16
ErN <t31092434@gmail.com>
2025-02-18 14:23:17 +0700

    first commit

---
+++ Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mar
+{
+    class Core
+    {
+        public static MarathonSkillsEntities DB = new MarathonSkillsEntities();
+    }
+}
